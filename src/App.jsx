146f5e3eb5226d000e8ed83415d78f95dcddc7fa
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-10-16 15:50:38 +0500

    Merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import { useState } from "react";
 
 import './App.css';
-import Summary from './components/Summary';
-import Dashboard from './components/Dashboard';
-import DataProvider from './context/dataProvider';
+
 
 import "./App.css";
-import userData from "./data/user1.json";
 // import Routes from "react-router-dom";
 import Router from "./Routes.jsx/AppRouter";
 function App() {
   return (
     // <>
-    //   <DataProvider>
-    //     <Dashboard />
-    //   </DataProvider>
+
     // </>
     // <div className="app grid grid-cols-5 grid-rows-5">
     //   <div className="col-span-full bg-gray-700 text-white p-6 text-center font-bold text-2xl">
