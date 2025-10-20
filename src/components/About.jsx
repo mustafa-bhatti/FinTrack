c461ad5f9ba5c504ac3747937b420541bc4a0645
ahmedbilal019 <ahmed258bilal@gmail.com>
2025-10-20 14:59:41 +0500

    creating signUp and LogIn component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import videoOfAbout from "../assets/about-video.mp4";
 
 function About() {
   return (
@@ -7,8 +8,8 @@ function About() {
       className="flex flex-col text-center align-center justify-center pt-[90px] pb-[90px]"
     >
       <h2 className="text-center text-3xl mb-[20px] font-semibold">About Us</h2>
-      <div className="flex align-center justify-center">
-        <div className="w-[50%]">
+      <div className="flex align-center justify-evenly">
+        <div className="w-[40%] flex align-center justify-left mt-[40px] text-left">
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
             accusantium quas quasi error iste ducimus velit facilis provident
@@ -16,11 +17,25 @@ function About() {
             consectetur adipisicing elit. Aperiam aut incidunt veritatis vero
             nisi praesentium, atque tempora temporibus nulla quis fugit
             repellendus ea non ipsum doloremque impedit cupiditate itaque
-            voluptatibus.
+            voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Nulla natus reprehenderit hic beatae, incidunt mollitia iste
+            quasi, atque repudiandae porro quisquam tempora, harum autem
+            corrupti nihil laudantium. Numquam provident odit incidunt magni
+            ipsam.
           </p>
         </div>
+
         <div>
-zx        </div>
+          <video
+            loop
+            autoPlay
+            muted
+            playsInline
+            className="w-[450px] rounded-xl shadow-lg"
+          >
+            <source src={videoOfAbout} />
+          </video>
+        </div>
       </div>
     </div>
   );
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,42 +1,64 @@
 import React from "react";
-
+import { PiChartPieSliceFill } from "react-icons/pi";
+import { HiCurrencyDollar } from "react-icons/hi";
+import { IoTimer } from "react-icons/io5";
 function Features() {
   return (
     <div
-      className="flex flex-col align-center justify-center pt-[90px] pb-[90px] "
+      className="flex flex-col align-center justify-center pt-[90px] pb-[90px]"
       id="features"
     >
       <h2 className="text-center text-3xl mb-[20px] font-semibold">Features</h2>
-      <div className="flex align-center justify-center gap-10 ml-[50px] mr-[50px]">
-        <div className="bg-white flex flex-col gap-4 align-center justify-center text-center pt-[70px] pb-[70px] pl-[20px] pr-[20px] rounded-md shadow-xl/10 cursor-pointer hover:inset-shadow-sm hover:shadow-xl/20">
+      <div className="flex align-center justify-center gap-10 ml-[50px] mr-[50px] mt-[50px] mb-[50px]">
+        <div className=" flex flex-col gap-4 align-center justify-center text-center pl-[20px] pr-[20px] rounded-md shadow-xl/10 cursor-pointer bg-gray-100 hover:inset-shadow-sm hover:shadow-xl/20  hover:bg-blue-400 hover:text-white ">
           <div className="flex align-center justify-center">
-            <img src="" alt="cardImg" />
+            <PiChartPieSliceFill
+              color="yellow"
+              size="100px"
+              className="mt-[-50px] bg-gray-300 rounded-[50px]"
+            />
           </div>
-          <h1 className="font-semibold  text-xl">Feature1</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            natus tempora reiciendis?
-          </p>
+          <div className="pt-[40px] pb-[40px] flex flex-col gap-7">
+            <h1 className="font-semibold  text-xl">Feature1</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              natus tempora reiciendis?
+            </p>
+          </div>
         </div>
-        <div className="bg-white flex flex-col gap-4 align-center justify-center text-center pt-[50px] pb-[50px] pl-[20px] pr-[20px] rounded-md shadow-xl/10 cursor-pointer hover:inset-shadow-sm hover:shadow-xl/20">
+        <div className=" flex flex-col gap-4 align-center justify-center text-center pl-[20px] pr-[20px] rounded-md shadow-xl/10 cursor-pointer bg-gray-100 hover:inset-shadow-sm hover:shadow-xl/20  hover:bg-blue-400 hover:text-white">
           <div className="flex align-center justify-center ">
-            <img src="" alt="cardImg" />
+            <HiCurrencyDollar
+              color="green"
+              size="100px"
+              className="mt-[-50px] bg-gray-300 rounded-[50px]"
+            />
           </div>
-          <h1 className="font-semibold text-xl">Feature2</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            natus tempora reiciendis?
-          </p>
+          <div className="pt-[40px] pb-[40px] flex flex-col gap-7">
+            {" "}
+            <h1 className="font-semibold text-xl">Feature2</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              natus tempora reiciendis?
+            </p>
+          </div>
         </div>
-        <div className="bg-white flex flex-col gap-4 align-center justify-center text-center pt-[50px] pb-[50px] pl-[20px] pr-[20px] rounded-md shadow-xl/10 cursor-pointer hover:inset-shadow-sm hover:shadow-xl/20">
+        <div className=" flex flex-col gap-4 align-center justify-center text-center pl-[20px] pr-[20px] rounded-md shadow-xl/10 cursor-pointer bg-gray-100 hover:inset-shadow-sm hover:shadow-xl/20 hover:bg-blue-400 hover:text-white">
           <div className="flex align-center justify-center">
-            <img src="" alt="cardImg" />
+            <IoTimer
+              color="blue"
+              size="100px"
+              className="mt-[-50px] bg-gray-300 rounded-[50px]"
+            />
           </div>
-          <h1 className="font-semibold text-xl">Feature3</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            natus tempora reiciendis?
-          </p>
+          <div className="pt-[40px] pb-[40px] flex flex-col gap-7">
+            {" "}
+            <h1 className="font-semibold text-xl">Feature3</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              natus tempora reiciendis?
+            </p>
+          </div>
         </div>
       </div>
     </div>
