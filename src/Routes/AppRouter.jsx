6b01921313ba2f04d3e1ba923aad72bfa703ad61
Ghulam Mustafa Bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-10-24 15:00:14 +0500

    merge

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "../components/LandingPage";
-import SignUp from "../components/SignUp";
-import Dashboard from "../components/Dashboard";
-import DataProvider from "../context/dataProvider";
-import Login from "../components/Login";
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from '../components/LandingPage';
@@ -15,7 +6,6 @@ import Dashboard from '../components/Dashboard';
 import DataProvider from '../context/dataProvider';
 import Login from '../components/Login';
 import TransanctionPage from '../components/TransanctionPage';
->>>>>>> ed8f7d24e866be67f89822ec3df6a651f5573881
 function AppRouter() {
   return (
     <Router>
@@ -25,14 +15,10 @@ function AppRouter() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
-          <Route path="/" element={<LandingPage />} />
-=======
           <Route
             path="/dashboard/transactions"
             element={<TransanctionPage />}
           />
->>>>>>> ed8f7d24e866be67f89822ec3df6a651f5573881
         </Routes>
       </DataProvider>
     </Router>
