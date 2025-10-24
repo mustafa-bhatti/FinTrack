5858cd7ebd9f101e84f308de0c2a53bcc00a7c9a
Ghulam Mustafa Bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-10-24 09:29:33 +0500

    Added two columns of income and expenses transactions

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -2,25 +2,53 @@ import React, { useContext } from 'react';
 import TransactionItem from './transactionItem';
 import { DataContext } from '../context/data';
 
-export function TransactionList() {
-  const { user} = useContext(DataContext);
+export function TransactionList({ name = 'Transactions' }) {
+  const { user } = useContext(DataContext);
 
   return (
-      <div className="flex flex-col gap-3 col-2 w-full p-2 flex-1">
-        <h1 className='font-bold'>Transactions</h1>
-        {user.transactions.map((transaction, index) => (
-          <TransactionItem
-            key={index}
-            category={transaction.category}
-            source={transaction.source}
-            value={transaction.amount}
-            date={transaction.date}
-            type={transaction.type}
-            currency={user.currency}
-          />
-        ))}
-      </div>
+    <div className="flex flex-col gap-3 col-2 w-full p-2 flex-1">
+      <h1 className="font-bold">{name}</h1>
+      {user.transactions.map((transaction, index) => {
+        if (name == 'Income' && transaction.type == 'income') {
+          // console.log();
+          return (
+            <TransactionItem
+              key={index}
+              category={transaction.category}
+              source={transaction.source}
+              value={transaction.amount}
+              date={transaction.date}
+              type={transaction.type}
+              currency={user.currency}
+            />
+          );
+        } else if (name == 'Transactions') {
+          console.log(name);
+          return (
+            <TransactionItem
+              key={index}
+              category={transaction.category}
+              source={transaction.source}
+              value={transaction.amount}
+              date={transaction.date}
+              type={transaction.type}
+              currency={user.currency}
+            />
+          );
+        } else if (name == 'Expenses' && transaction.type == 'expense') {
+          return (
+            <TransactionItem
+              key={index}
+              category={transaction.category}
+              source={transaction.source}
+              value={transaction.amount}
+              date={transaction.date}
+              type={transaction.type}
+              currency={user.currency}
+            />
+          );
+        }
+      })}
+    </div>
   );
 }
-
-
