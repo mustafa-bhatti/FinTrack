386a8c39ec31f4380d3b5d0a74af92c8adaeadab
Ahmed Bilal <ahmed258bilal@gmail.com>
2025-11-03 14:40:37 +0500

    adding animations in different components

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "../components/LandingPage";
@@ -6,16 +5,7 @@ import SignUp from "../components/SignUp";
 import Dashboard from "../components/Dashboard";
 import DataProvider from "../context/dataProvider";
 import Login from "../components/Login";
-=======
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from '../components/LandingPage';
-import SignUp from '../components/SignUp';
-import Dashboard from '../components/Dashboard';
-import DataProvider from '../context/dataProvider';
-import Login from '../components/Login';
-import TransanctionPage from '../components/TransanctionPage';
->>>>>>> ed8f7d24e866be67f89822ec3df6a651f5573881
+import TransanctionPage from "../components/TransanctionPage";
 function AppRouter() {
   return (
     <Router>
@@ -25,14 +15,11 @@ function AppRouter() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
           <Route path="/" element={<LandingPage />} />
-=======
           <Route
             path="/dashboard/transactions"
             element={<TransanctionPage />}
           />
->>>>>>> ed8f7d24e866be67f89822ec3df6a651f5573881
         </Routes>
       </DataProvider>
     </Router>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <div className="text-center pt-[13px] pb-[13px] bg-green-700 text-white w-[100%]">
+      <div className="text-center pt-[13px] pb-[13px] bg-gradient-to-b from-[#065f46] to-[#065841]  text-white w-[100%]">
         <p>
           FinTrack is developed by Gulam Mustafa & Ahmed Bilal. All Right
           Reserved &copy; 2025
